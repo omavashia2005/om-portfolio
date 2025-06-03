bbf5d15647b8b46fac6e4a4b5a502e70127ceab4
omavashia <omavashia1233@gmail.com>
2025-06-03 15:15:50 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,13 @@
 import Link from "next/link";
-import Head from "next/head";
-export default function Navbar(){
-    return(
-<nav className="sticky top-0 left-0 w-full bg-black text-white flex space-x-5 p-4 gap-50 z-50">
-        <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-            <Link href="#home">Home</Link>
-            <Link href="#about">About</Link>
-            <Link href="#experience">Experience</Link>
-            <Link href="#projects">Projects</Link>
-            <Link href="#contact">Contact</Link>
-        </nav>
-    )
+
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 left-0 w-full bg-black text-white flex flex-wrap justify-center md:justify-start gap-4 md:gap-8 p-4 z-50">
+      <Link href="#home" className="hover:underline">Home</Link>
+      <Link href="#about" className="hover:underline">About</Link>
+      <Link href="#experience" className="hover:underline">Experience</Link>
+      <Link href="#projects" className="hover:underline">Projects</Link>
+      <Link href="#contact" className="hover:underline">Contact</Link>
+    </nav>
+  );
 }
