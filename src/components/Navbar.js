d28def30565c6314e05e9015937a52064d4b066d
omavashia <omavashia1233@gmail.com>
2025-06-03 15:13:02 +0530

    navbar fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
-
+import Head from "next/head";
 export default function Navbar(){
     return(
 <nav className="sticky top-0 left-0 w-full bg-black text-white flex space-x-5 p-4 gap-50 z-50">
+        <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
             <Link href="#home">Home</Link>
             <Link href="#about">About</Link>
             <Link href="#experience">Experience</Link>
